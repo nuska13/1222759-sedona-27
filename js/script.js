1a38f7851eb4d9d8ec774fb07e3d40179b678c67
Yana <nuska13@mail.ru>
2020-03-24 22:39:33 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 var btn = document.querySelector(".modal-form-button");
 var form = document.querySelector(".modal-form");
-btn.addEventListener("click", function (evt) {
-evt.preventDefault();
-if (form.classList.contains("modal-form-visible")) {
-form.classList.add("modal-form-invisible");
-form.classList.remove("modal-form-visible");
-}
-else {
-form.classList.add("modal-form-visible");
-form.classList.remove("modal-form-invisible");
-}
+btn.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (form.classList.contains("modal-form-visible")) {
+    form.classList.add("modal-form-invisible");
+    form.classList.remove("modal-form-visible");
+  } else {
+    form.classList.add("modal-form-visible");
+    form.classList.remove("modal-form-invisible");
+  }
 });
